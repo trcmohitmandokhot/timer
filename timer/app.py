85cc53dcd5153e8de29d7531875c09d94b86851a
trcmohitmandokhot <mohitmandokhot@gmail.com>
2023-11-20 23:04:25 -0500

    Implement app in pure python

--- timer/app.py
+++ timer/app.py
@@ -1,23 +1,42 @@
-import sys
+import time
 
-from PyQt5 import QtWidgets
-
-from ui.timer_ui import Ui_MainWindow
+CLOCK_INTERVAL = 1
 
 
-class MyMainWindow(QtWidgets.QWidget):
-    def __init__(self) -> None:
-        super().__init__()
-        pass
+def check_validity_timer_start(timer_value):
+    if isinstance(timer_value, int) and timer_value > 0:
+        return True
+    else:
+        print("timer_start user entry invalid")
+        return False
 
 
-# class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-#     def __init__(self):
-#         super().__init__()
-#         self.setupUi(self)
+def update_display(val):
+    print(val)
 
 
-app = QtWidgets.QApplication([])
-window = MyMainWindow()
-window.show()
-app.exec_()
+def countdown_logic(timer_start, timer_go, valid_timer_start):
+    # Countdown Logic
+    timer_now = timer_start
+
+    while timer_go and valid_timer_start:
+        update_display(timer_now)
+        timer_now = timer_now - 1
+        if timer_now < 0:
+            timer_go = False
+            break
+        else:
+            time.sleep(CLOCK_INTERVAL)  # 1ms accuracy.
+
+
+def run_countdown_timer():
+    # User Input
+    timer_start = 10
+    timer_go = True
+
+    valid_timer_start = check_validity_timer_start(timer_start)
+    countdown_logic(timer_start, timer_go, valid_timer_start)
+
+
+if __name__ == "__main__":
+    run_countdown_timer()
